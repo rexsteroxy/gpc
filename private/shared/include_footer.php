92b3bf5d488d0620569ddcbff365bf61ac1a1620
rexsteroxy <rexsteroxy22@gmail.com>
2019-03-17 14:23:20 +0100

    worked on the footer

--- private/shared/include_footer.php
+++ private/shared/include_footer.php
@@ -4,10 +4,10 @@
 		<div class="container">
 			<div class="row text-center text-xs-center text-sm-left text-md-left">
 				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5><a href="contact.php" style ="color: white">Contact us</a></h5>
+					<h5><a href="#" style ="color: white">Contact us</a></h5>
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-4">
-                <h5><a href="about.php" style ="color: white">About us</a></h5>
+                <h5><a href="#" style ="color: white">About us</a></h5>
 				</div>
 				<div class="col-xs-12 col-sm-4 col-md-4">
                 <h5><a href="signup.php" style ="color: white">Register </a></h5>
